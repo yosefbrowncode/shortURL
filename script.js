1ab3813fe32c12f85ee8c3aa281a93fe8fdb80a3
Yosef Brown <yosef@aklaunch.com>
2022-08-24 03:28:18 +0300

    Almost done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let inputLink = document.getElementById("shortForm");
+let errorMessageRemove = document.getElementById("errorMessage");
 
 // Execute a function when the user presses a key on the keyboard
 inputLink.addEventListener("keypress", function (event) {
@@ -13,6 +14,13 @@ inputLink.addEventListener("keypress", function (event) {
 function littleLink() {
   let geegle = document.getElementById("shortForm").value;
   let resetVal = document.getElementById("shortForm");
+  let errorMessageEmpty = document.getElementById("errorMessage");
+  if (geegle.length == 0) {
+    errorMessageEmpty.innerHTML = "Please add a link";
+
+    resetVal.focus();
+    resetVal.classList.add("focusChange");
+  }
   const Yes = `https://api.shrtco.de/v2/shorten?url=${geegle}`;
   axios
     .get(Yes)
@@ -21,8 +29,6 @@ function littleLink() {
       let addLink = document.getElementById("addToList");
       let oldLink = response.data.result.original_link;
       let shortLink = response.data.result.full_short_link;
-      console.log(response.data.result.full_short_link);
-      console.log(response.data.result.original_link);
       addLink.classList.add("giveMargin");
       const li = document.createElement("li");
       li.classList.add("mb-4");
@@ -34,8 +40,8 @@ function littleLink() {
         </div>
         <div class="col-12 d-block  d-lg-none"><hr></div>
           <div class="col colDisplay">
-          <h4 class="my-auto coloredLink">${shortLink}</h4>
-          <button  type="button" class="btn btn-primary float-right linkBtn me-0 me-lg-4 btnHover" onClick="copyLink()" >copy</button>
+          <h4 id="shorty" class="my-auto coloredLink">${shortLink}</h4>
+          <button  type="button" class="btn btn-primary float-right linkBtn me-0 me-lg-4 btnHover" onClick="copyLink(this); this.blur();" >Copy</button>
           </div>
         </div>
       </div>
@@ -43,10 +49,37 @@ function littleLink() {
       li.innerHTML = newLink;
       addLink.prepend(li);
       console.log(geegle);
+      errorMessage.innerHTML = "";
     })
     .catch(function (error) {
       // handle error
-      errorMessage.innerHTML = "* Please check your link";
+      if (geegle.length > 0) {
+        resetVal.focus();
+        resetVal.classList.add("focusChange");
+        errorMessage.innerHTML = "Please check your link";
+      }
     });
   resetVal.value = "";
 }
+
+function copyLink(e) {
+  let copyBtn = e;
+  let shortCopy = e.parentNode.children[0];
+  shortCopy = shortCopy.innerHTML;
+  copyBtn.classList.add("copiedBtn");
+  copyBtn.innerHTML = "Copied!";
+  copyTextToClipboard(shortCopy);
+}
+
+async function copyTextToClipboard(text) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {
+    alert("Error in copying text: ", err);
+  }
+}
+
+inputLink.addEventListener("input", (event) => {
+  inputLink.classList.remove("focusChange");
+  errorMessageRemove.innerHTML = "";
+});
